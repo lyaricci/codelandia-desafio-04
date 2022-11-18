3779a3957c7c18ea42a881bd3956b985f9aa914c
Nathálya Ricci <nathalyaricci@gmail.com>
2022-11-18 19:44:49 -0300

    commit inicial

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./**/*.html"],
+	purge: ["*.html"],
+	theme: {
+		extend: {
+			colors: {
+				"green-light": "#04C35C",
+				"purple-light": "#2B6CB0",
+				"white-200": "#F7FAFC",
+				"gray": "#1A202C",
+			},
+			fontFamily: {
+				lato: ["'Lato', sans"],
+				merriweather: ["'Merriweather', serif"],
+			},
+		},
+	},
+	plugins: [],
+};
