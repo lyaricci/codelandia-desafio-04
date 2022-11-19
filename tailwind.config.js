a5af0ae0db486cc19eff277784511bde427f48cb
Nathálya Ricci <nathalyaricci@gmail.com>
2022-11-19 12:36:29 -0300

    adiciona cores hover

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ module.exports = {
 				"purple-light": "#2B6CB0",
 				"white-200": "#F7FAFC",
 				"gray": "#1A202C",
+				"border-input": "#E8E8E8",
+				"btn-hover-green": "#14d96e",
+				"btn-hover-gray": "#303745",
 			},
 			fontFamily: {
 				lato: ["'Lato', sans"],
